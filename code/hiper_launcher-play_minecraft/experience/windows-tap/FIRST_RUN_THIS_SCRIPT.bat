969c24b66734c13c3bd7de0e9d4c9964f56d84d7
zkitefly <64117916+zkitefly@users.noreply.github.com>
2022-07-25 10:14:18 +0800

    Add files via upload

---
+++ code/hiper_launcher-play_minecraft/experience/windows-tap/FIRST_RUN_THIS_SCRIPT.bat
@@ -0,0 +1,13 @@
+@echo off
+echo. ------------------------------------
+echo. =         正在安装虚拟网卡         =
+echo. ------------------------------------
+@echo on
+@rem Add a new TAP virtual ethernet adapter
+
+cd /d %~dp0
+
+tap_install.exe install OemVista.inf tap0901
+
+timeout /t 5
+
