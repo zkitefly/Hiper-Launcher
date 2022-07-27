0ce6ec8e80d1d67adf0b91b2812abf6669d233f0
zkitefly <64117916+zkitefly@users.noreply.github.com>
2022-07-28 01:34:58 +0800

    Add files via upload

--- code/hiper_launcher-play_minecraft/home.bat
+++ code/hiper_launcher-play_minecraft/home.bat
@@ -10,11 +10,9 @@ del /f /s /q C:\Windows\Temp\update.bat
 
 goto home
 
-:home-0
-cls
-goto home
-
 :home
+
+cls
 
 color 1f
 
@@ -29,12 +27,16 @@ echo. 直接回车 或 输入 1 回车 即运行 Hiper 凭证版本
 echo.
 set /p mm=请输入 Hiper 启动模式：
 if “%mm%”==“0” call experience.bat
+
 if “%mm%”==“i” call installTab.bat
 if “%mm%”==“I” call installTab.bat
+
 if “%mm%”==“u” call update.bat
 if “%mm%”==“U” call update.bat
+
 if “%mm%”==“a” call about.bat
 if “%mm%”==“A” call about.bat
+
 if “%mm%”==“” call token.bat
 if “%mm%”==“1” call token.bat
 
