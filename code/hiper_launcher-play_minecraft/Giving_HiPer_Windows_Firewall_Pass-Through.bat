dd5661df87411f8ca940e7472ba01b73a36a41f5
zkitefly <64117916+zkitefly@users.noreply.github.com>
2022-07-29 00:21:39 +0800

    Add files via upload

--- code/hiper_launcher-play_minecraft/Giving_HiPer_Windows_Firewall_Pass-Through.bat
+++ code/hiper_launcher-play_minecraft/Giving_HiPer_Windows_Firewall_Pass-Through.bat
@@ -6,6 +6,12 @@ if not exist C:\Set_HiPer_Windows_Firewall_to_pass_successfully.txt goto add
 goto ok
 
 :add
+echo. -----------------------------------------------------------------------------------------------------------------------
+echo  =                                                                                                                     =
+echo. =                                   正在添加 HiPer 本体和 虚拟网卡 的防火墙通行权限                                   =
+echo  =                                                                                                                     =
+echo. -----------------------------------------------------------------------------------------------------------------------
+
 netsh advfirewall firewall add rule name="hiper-windows-i386" dir=in action=allow program="%appdata%\Roaming\hiper_launcher-play_minecraft\experience\hiper-windows-i386.exe" enable=yes
 
 netsh advfirewall firewall add rule name="hpr" dir=in action=allow program="%appdata%\Roaming\hiper_launcher-play_minecraft\token\hpr.exe" enable=yes
