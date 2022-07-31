7272c15f1252fe77f9f728ab64694faadb5e20af
zkitefly <64117916+zkitefly@users.noreply.github.com>
2022-07-31 18:51:47 +0800

    Add files via upload

--- code/hiper_launcher-play_minecraft/update.bat
+++ code/hiper_launcher-play_minecraft/update.bat
@@ -60,7 +60,7 @@ echo  正在检查程序更新，可能需要一些时间...
 echo.
 echo -------------------------------------------------------------------------------
 
-%appdata%\hiper_launcher-play_minecraft\wget.exe  -q -N -T 5 -P %appdata%\hiper_launcher-play_minecraft -S -T 5 https://raw.fastgit.org/zkitefly/Hiper-Launcher/main/code/hiper_launcher-play_minecraft/v.txt
+%appdata%\hiper_launcher-play_minecraft\wget.exe  -q -N -T 5 -P %appdata%\hiper_launcher-play_minecraft -S -T 5 https://raw.fastgit.org/zkitefly/Hiper-Launcher/main/code/hiper_launcher-play_minecraft/d-update.bat
 
 del /f /s /q %appdata%\hiper_launcher-play_minecraft\d-update.bat
 
